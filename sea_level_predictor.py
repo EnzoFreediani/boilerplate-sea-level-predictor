bbab62db52752c1ba2d06ac1ee124f95197d0f69
Enzo Frediani <enzofrediani@usp.br>
2025-08-30 00:52:51 +0000

    minha solucao

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
+import numpy as np
 
 def draw_plot():
     df = pd.read_csv("epa-sea-level.csv")
@@ -10,12 +11,15 @@ def draw_plot():
 
 
     regressao = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
-    x = [df.iloc[0,0], 2050]
+    x = np.arange(1880, 2051)
     y = [((i * regressao[0]) + regressao[1]) for i in x]
 
     regressao2 = linregress(df[df['Year'] >= 2000]['Year'], df[df['Year'] >= 2000]['CSIRO Adjusted Sea Level'])
-    x2 = [2000, 2050]
+    x2 = np.arange(2000, 2051)
     y2 = [((i * regressao2[0]) + regressao2[1]) for i in x2]
+
+    ax.plot(x,y)
+    ax.plot(x2,y2)
 
     ax.set_xlabel('Year')
     ax.set_title('Rise in Sea Level')
